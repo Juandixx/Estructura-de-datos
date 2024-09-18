991aacf128b81652f1b53b6aa2fdcda34840a04e
Juandixx <juandfm1801@gmail.com>
2024-09-18 15:37:26 -0500

    4_Variables_Globales

---
+++ VariablesGlobales.py
@@ -0,0 +1,16 @@
+#esta es la forma de definir las variables globales 
+x = "Awesome"
+#Las variables globales pueden ser utilizadas por todos, tanto dentro de funciones y exterior.
+
+def Funcion():
+    #Creamos una funcion y usamos la variable dentro de la funcion de esta manera 
+    print("phyton is ", x)
+Funcion()
+
+def Funcionx():
+    #tambien podemos crear una variable dentro de la funcion con el mismo nombre de la variable global
+    x = "Fantastic"
+    print("Phyton is ", x)
+Funcionx()
+
+print("Phyton is ", x)
