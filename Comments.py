c4d9dd8e597c21678fe110af7d9171d519f23c6b
Juandixx <juandfm1801@gmail.com>
2024-09-18 15:47:03 -0500

    1_COMMENTS

---
+++ Comments.py
@@ -0,0 +1,3 @@
+#Hola mundo para el curso de estructuras de datos basados en el lenguage de phyton
+#los comentarios se activan insertando el numeral (#) al rpincipio del texto
+print("Hello world")
