d045d29c83c27072ac6ca049337555612f0f7fea
Juandixx <juandfm1801@gmail.com>
2024-09-19 15:53:13 -0500

    12_CADENAS DE TEXTO

---
+++ CadenasDeTexto.py
@@ -0,0 +1,29 @@
+#CADENAS DE TEXTOS O STRINGS
+
+#Las cadenas en phyton estan rodeadas por comillas simples o dobles
+
+#Ejemplo
+
+print("Hola mundo")
+print('Hola mundo')
+
+#Vemos que no hay ningun problema en escribirla con cualquiera de las dos
+
+#Puede usar comillas dentro de una cadena, siempre que no coincidan con las comillas que rodean la cadena
+
+#Ejemplo
+print("Los float's o flotantes son un tipo de variable de phyton")
+print("El se llama 'Juan'")
+print('Su nombre es "Juan"')
+
+#Asignacion de una cadena a una variable de variable
+#la forma de hacerla es esta 
+a = "Hola"
+print(a)
+
+#Puede asignar una cadena multilínea a una variable utilizando tres comillas
+#Ejemplo
+
+a1 = """Los enteros son un tipo de dato utilizados en programacion"""
+print(a1)
+
