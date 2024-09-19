05036cbc67a51e8cad08351fb21f30a14bb26455
Juandixx <juandfm1801@gmail.com>
2024-09-19 15:40:42 -0500

    8_COMPLEX O COMPLEJOS

---
+++ Complex.py
@@ -0,0 +1,14 @@
+#Los números complejos se escriben con una "j" como parte imaginaria
+
+x = 3+5j
+y = 7j
+z = -7j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(x)
+print(y)
+print(z)
+
