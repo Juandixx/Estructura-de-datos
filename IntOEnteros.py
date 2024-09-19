74c573f3d54e44612bbb0df182e8c1e64a864999
Juandixx <juandfm1801@gmail.com>
2024-09-19 15:39:53 -0500

    7_ENTEROS O INT

---
+++ IntOEnteros.py
@@ -0,0 +1,16 @@
+#Iniciemos hablando de los int o enteros que es 
+#un numero entero positivo o negativo sin decimales
+
+#Ejemplo
+
+x = 1
+y = 1234567890
+z = -123456
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(x)
+print(y)
+print(z)
