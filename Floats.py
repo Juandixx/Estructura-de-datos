a6f7eddb4337f9aaa4d9acfe6cab9e9364e74bba
Juandixx <juandfm1801@gmail.com>
2024-09-19 15:40:17 -0500

    8_FLOAT O FLOTANTE

---
+++ Floats.py
@@ -0,0 +1,29 @@
+#El float o "numero de punto flotante " es un numero positivo o negativo
+# que contiene uno o mas decimales 
+
+x = 1.1113
+y = 1.0
+z = -28.08625
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(x)
+print(y)
+print(z)
+
+#Float tambien puede ser numeros cientificos, no mas utilizamos
+#la "e" para indicar la potencia de 10
+
+x1 = 15e3
+y1 = 28.87643E3
+z1 = -34.8e100
+
+print(type(x1))
+print(type(y1))
+print(type(z1))
+
+print(x1)
+print(y1)
+print(z1)
